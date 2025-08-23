ff4a37b5d71d9cb356e9a40aeee4d91c447d49a0
Benjamin Marwell <bmarwell@apache.org>
2025-08-23 21:24:09 +0200

    chore(ITs): fix format for LicenseIT

--- integration-tests/jreleaser-builtin/src/test/java/io/github/bmarwell/jdtfmt/its/jreleaser/builtin/LicenseIT.java
+++ integration-tests/jreleaser-builtin/src/test/java/io/github/bmarwell/jdtfmt/its/jreleaser/builtin/LicenseIT.java
@@ -1,12 +1,11 @@
 package io.github.bmarwell.jdtfmt.its.jreleaser.builtin;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
 
 class LicenseIT {
 
