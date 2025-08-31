b3a87858d3a775a25f50784d3b046bc3b6923e8d
Benjamin Marwell <bmarwell@apache.org>
2025-08-31 14:41:39 +0200

    feat(importorder): add importorder sorting
    Refs: #34
    Fixes #34
    
    # Conflicts:
    #	cli/src/test/java/io/github/bmarwell/jfmt/commands/PrintTest.java

--- cli/src/main/java/io/github/bmarwell/jfmt/commands/AbstractCommand.java
+++ cli/src/main/java/io/github/bmarwell/jfmt/commands/AbstractCommand.java
@@ -14,13 +14,24 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.Callable;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.ToolFactory;
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.ASTParser;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.ImportDeclaration;
+import org.eclipse.jdt.core.dom.Modifier;
 import org.eclipse.jdt.core.formatter.CodeFormatter;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.Document;
@@ -78,7 +89,7 @@ public abstract class AbstractCommand implements Callable<Integer> {
 
         try (var javaSource = Files.newInputStream(javaFile)) {
             final String sourceCode = new String(javaSource.readAllBytes(), StandardCharsets.UTF_8);
-            final String revisedSourceCode = createRevisedSourceCode(formatter, sourceCode);
+            final String revisedSourceCode = createRevisedSourceCode(formatter, javaFile, sourceCode);
 
             final List<String> originalSourceLines = List.of(sourceCode.split("\n"));
             final List<String> revisedSourceLines = List.of(revisedSourceCode.split("\n"));
@@ -94,7 +105,7 @@ public abstract class AbstractCommand implements Callable<Integer> {
             );
         } catch (IOException ioException) {
             throw new UncheckedIOException(ioException);
-        } catch (org.eclipse.jface.text.BadLocationException ble) {
+        } catch (BadLocationException | CoreException ble) {
             getWriter().warn("Error formatting file", javaFile.toString());
             throw new IllegalStateException(ble);
         }
@@ -122,8 +133,69 @@ public abstract class AbstractCommand implements Callable<Integer> {
         return ToolFactory.createCodeFormatter(config);
     }
 
-    static String createRevisedSourceCode(CodeFormatter formatter, String sourceCode)
-        throws BadLocationException {
+    static String createRevisedSourceCode(CodeFormatter formatter, Path javaFile, String sourceCode)
+        throws BadLocationException, CoreException {
+        CompilationUnit compilationUnit = getCompilationUnitFrom(sourceCode, javaFile);
+
+        if (compilationUnit.getProblems() != null && compilationUnit.getProblems().length > 0) {
+            Stream.of(compilationUnit.getProblems())
+                .forEach(System.err::println);
+            throw new IllegalStateException(
+                "CompilationUnit problens: " + Arrays.asList(compilationUnit.getProblems())
+            );
+        }
+
+        // cannot use ImportRewrite which requires an Eclipse Workspace
+        // Collect imports
+        @SuppressWarnings("unchecked")
+        List<ImportDeclaration> imports = new ArrayList<>(compilationUnit.imports());
+
+        StringBuilder sb = new StringBuilder();
+        List<ImportDeclaration> staticImports = new ArrayList<>();
+        List<ImportDeclaration> normalImports = new ArrayList<>();
+        List<ImportDeclaration> javaImports = new ArrayList<>();
+
+        for (ImportDeclaration id : imports) {
+            String name = id.getName().getFullyQualifiedName();
+            if ((Modifier.isStatic(id.getModifiers())))
+                staticImports.add(id);
+            else if (name.startsWith("java"))
+                javaImports.add(id);
+            else
+                normalImports.add(id);
+        }
+
+        Consumer<List<ImportDeclaration>> appendGroup = list -> {
+            for (ImportDeclaration id : list) {
+                // includes trailing newline
+                sb.append(id.toString());
+            }
+
+            // extra blank line between groups
+            sb.append("\n");
+        };
+
+        appendGroup.accept(staticImports);
+        appendGroup.accept(normalImports);
+        appendGroup.accept(javaImports);
+
+        // Replace imports list manually
+        int importStart = compilationUnit.imports().isEmpty()
+            ? 0
+            : ((ImportDeclaration) compilationUnit.imports().getFirst()).getStartPosition();
+        int importEnd = compilationUnit.imports().isEmpty()
+            ? 0
+            : compilationUnit.imports()
+                .stream()
+                .mapToInt(id -> ((ASTNode) id).getStartPosition() + ((ASTNode) id).getLength())
+                .max()
+                .orElse(0);
+
+        final IDocument docImport = new Document(sourceCode);
+        docImport.replace(importStart, importEnd - importStart, sb.toString());
+        TextEdit importRewrite = compilationUnit.rewrite(docImport, null);
+        importRewrite.apply(docImport);
+
         final TextEdit edit = formatter.format(
             CodeFormatter.K_COMPILATION_UNIT,
             sourceCode,
@@ -135,10 +207,30 @@ public abstract class AbstractCommand implements Callable<Integer> {
 
         Objects.requireNonNull(edit, "Formatting edits must not be null.");
 
-        final IDocument doc = new Document(sourceCode);
-        edit.apply(doc);
+        edit.apply(docImport);
 
-        return doc.get();
+        return docImport.get();
+    }
+
+    private static CompilationUnit getCompilationUnitFrom(String sourceCode, Path javaFile) {
+        // extract package from file
+        ASTParser parser = ASTParser.newParser(AST.getJLSLatest());
+        parser.setSource(sourceCode.toCharArray());
+        parser.setUnitName(javaFile.getFileName().toString());
+        parser.setKind(ASTParser.K_COMPILATION_UNIT);
+
+        // Configure compiler options
+        Map<String, String> options = JavaCore.getOptions();
+        options.put(JavaCore.COMPILER_SOURCE, String.valueOf(AST.getJLSLatest()));
+        options.put(JavaCore.COMPILER_COMPLIANCE, String.valueOf(AST.getJLSLatest()));
+        options.put(JavaCore.COMPILER_CODEGEN_TARGET_PLATFORM, String.valueOf(AST.getJLSLatest()));
+        parser.setCompilerOptions(options);
+
+        org.eclipse.jdt.core.dom.CompilationUnit compilationUnit = (CompilationUnit) parser.createAST(null);
+        compilationUnit.recordModifications();
+
+        // Create a package + CU
+        return compilationUnit;
     }
 
     public OutputWriter getWriter() {
---
+++ cli/src/test/java/io/github/bmarwell/jfmt/commands/PrintTest.java
@@ -0,0 +1,33 @@
+package io.github.bmarwell.jdtfmt.commands;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class PrintTest extends AbstractCommandTest {
+
+    @Test
+    void can_print_with_sorted_imports() {
+        // given
+        var args = new String[] { "print", "target/test-classes/diff/SomeRecord.java" };
+        String importDec = """
+                           import static io.github.bmarwell.jdtfmt.format.FormatterMode.DIFF;
+
+                           import io.github.bmarwell.jdtfmt.writer.OutputWriter;
+
+                           import java.lang.String;
+                           """;
+
+        // when
+        var executionResult = doExecute(args);
+
+        // then
+        assertEquals(1, executionResult.returncode());
+        assertFalse(executionResult.stdout().getFirst().startsWith("import java.lang.String;"));
+
+        String formattedSource = String.join("\n", executionResult.stdout());
+        assertTrue(formattedSource.startsWith(importDec));
+    }
+}
